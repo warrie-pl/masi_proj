89152071b42f19158e64318169ce4efb724bced4
Warrie <megaemil9@gmail.com>
2024-06-05 14:51:16 +0200

    fourth draft, now it's looking like something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from tkinter import StringVar, IntVar
+from tkinter import StringVar, IntVar, messagebox
 
 
 class DrawingFrame(ctk.CTkFrame):
@@ -30,9 +30,6 @@ class App(ctk.CTk):
         self.seq_value = IntVar(value=0)
         self.uniterm_value = IntVar(value=0)
 
-        def get_sep_value(canv):
-            canv.create_text(200, 100, text=self.sep_value.get(), fill="black", font="Helvetica 14 bold")
-
         self.options_frame = OptionsFrame(master=self)
         self.options_frame.grid(row=0, column=0, rowspan=2, sticky=ctk.NS)
 
@@ -51,24 +48,22 @@ class App(ctk.CTk):
 
         self.first_param_header = ctk.CTkLabel(master=self.options_frame, text="Pierwszy parametr")
         self.first_param_header.pack()
-        self.first_param_input = ctk.CTkTextbox(master=self.options_frame, width=self.winfo_width() // 4, height=20)
+        self.first_param_input = ctk.CTkEntry(master=self.options_frame, width=self.winfo_width() // 4, height=20)
         self.first_param_input.pack()
 
         self.sec_param_header = ctk.CTkLabel(master=self.options_frame, text="Drugi parametr")
         self.sec_param_header.pack()
-        self.sec_param_input = ctk.CTkTextbox(master=self.options_frame, width=self.winfo_width() // 4, height=20)
+        self.sec_param_input = ctk.CTkEntry(master=self.options_frame, width=self.winfo_width() // 4, height=20)
         self.sec_param_input.pack()
 
         self.sep_frame = ctk.CTkFrame(master=self.options_frame, fg_color="transparent")
         self.sep_frame.pack(pady=10, side=ctk.TOP)
 
-        self.first_sep_choice = ctk.CTkRadioButton(master=self.sep_frame, text=",", variable=self.sep_value,
-                                                   command=lambda: get_sep_value(self.left_drawing_frame.canvas),
-                                                   value=",", width=55)
+        self.first_sep_choice = ctk.CTkRadioButton(master=self.sep_frame, text=",", variable=self.sep_value, value=",",
+                                                   width=55)
         self.first_sep_choice.pack(side=ctk.LEFT, pady=0)
-        self.sec_sep_choice = ctk.CTkRadioButton(master=self.sep_frame, text=";", variable=self.sep_value,
-                                                 command=lambda: get_sep_value(self.right_drawing_frame.canvas),
-                                                 value=";", width=45)
+        self.sec_sep_choice = ctk.CTkRadioButton(master=self.sep_frame, text=";", variable=self.sep_value, value=";",
+                                                 width=45)
         self.sec_sep_choice.pack(side=ctk.RIGHT)
 
         self.seq_frame = ctk.CTkFrame(master=self.options_frame, fg_color="transparent")
@@ -91,8 +86,26 @@ class App(ctk.CTk):
                                                  value=1, width=35)
         self.second_uniterm.pack(side=ctk.RIGHT)
 
+        def draw_uniterm(canv, uniterm):
+            if self.seq_value == 0:
+                pass
+            elif self.seq_value == 1:
+                pass
+
+        def choose_uniterm():
+            uniterm = self.first_param_input.get()+"\n"+self.sep_value.get()+"\n"+self.sec_param_input.get()
+            if self.first_param_input.get() == "" or self.sec_param_input.get() == "":
+                messagebox.showinfo("Uniterm", "Uniterm wymaga dwóch parametrów!")
+            else:
+                if self.uniterm_value.get() == 0:
+                    self.left_drawing_frame.canvas.create_text(200, 100, text=uniterm, fill="black",
+                                                               font="Arial 14 bold")
+                elif self.uniterm_value.get() == 1:
+                    self.right_drawing_frame.canvas.create_text(200, 100, text=uniterm, fill="black",
+                                                                font="Arial 14 bold")
+
         self.add_uniterm = ctk.CTkButton(master=self.options_frame, text="Dodaj uniterm",
-                                         command=lambda: print(self.first_param_input.get("0.0", "end")))
+                                         command=choose_uniterm)
         self.add_uniterm.pack(pady=10)
 
 
